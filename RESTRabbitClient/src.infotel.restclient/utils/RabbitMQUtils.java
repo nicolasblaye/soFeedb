6f8dc66ea02af6675089c502fe1ef6585e3a5027
nicolasblaye <nicolas_blaye@hotmail.fr>
2016-05-26 14:03:41 +0200

    all set for the contest

--- RESTRabbitClient/src.infotel.restclient/utils/RabbitMQUtils.java
+++ RESTRabbitClient/src.infotel.restclient/utils/RabbitMQUtils.java
@@ -10,6 +10,8 @@ import com.rabbitmq.client.ConnectionFactory;
 public class RabbitMQUtils {
 	private static final String RPC_QUEUE_NAME = "rpc_queue";
 	private static Channel RPCChannel;
+	private static final String NOTIFICATION_QUEUE_NAME = "notification";
+	private static Channel NotificationChannel;
 	
 	public static Channel getRPCChannel() throws IOException, TimeoutException{
 		if (RPCChannel == null){	
@@ -25,6 +27,21 @@ public class RabbitMQUtils {
 		}
 		return RPCChannel;
 		
+	}
+
+	public static Channel getNotificationChannel() throws IOException, TimeoutException {
+		if (NotificationChannel == null){	
+			ConnectionFactory factory = new ConnectionFactory();
+			factory.setHost("192.168.0.81");
+	
+			Connection connection = factory.newConnection();
+			NotificationChannel = connection.createChannel();
+	
+			NotificationChannel.queueDeclare(NOTIFICATION_QUEUE_NAME, false, false, false, null);
+	
+			NotificationChannel.basicQos(1);
+		}
+		return RPCChannel;
 	}
 	
 
